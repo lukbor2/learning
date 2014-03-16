78da9a6465c5c93553335659647e82bdf53194d4
lukbor2 <lukbor2@gmail.com>
2014-03-15 19:38:34 -0700

    From Eclipse 3/15 evening.

--- EloquentJavaScript/js/Sokoban.js
+++ EloquentJavaScript/js/Sokoban.js
@@ -1,7 +1,5 @@
 /**
  * Square is an object with properties and methods.
- * Looking at the example in Ch 13 it looks like the method "construct" is called every time a new Square object is created
- * I did not expect that and I did not understand why it happens
  */
 
 var Square = {
@@ -20,10 +18,10 @@ var Square = {
 				this.content = "boulder";
 			else if (character == "@")
 				this.content = "player";
-
+			/* Note the images for wall, empty and exit are managed via the css style sheet, not here */
 			if (this.content != null) {
-				var image = dom("IMG", {src: "img/sokoban/" +
-					this.content + ".gif"});
+				var image = dom("IMG", {src: "img/sokoban/" + this.content + ".gif"});
+				console.log("img/sokoban/" + this.content + ".gif");
 				this.tableCell.appendChild(image);
 			}
 		},
@@ -55,11 +53,17 @@ var Square = {
 		}
 };
 
+/**
+ * SokobenField has a constructor to which is given an object from the sokobanLevels array, 
+ * and is responsible for building both a table of DOM nodes and a grid of Square objects.
+ * SokobanField will also take care of the details of moving the player and boulders around.
+ */
+
 var SokobanField = {
 		construct: function(level) {
-			var tbody = dom("TBODY");
+			var tbody = dom("TBODY"); /* TBODY is an HTML tag; Table Body. So tbody is a DOM node (the function dom returns a node) */
 			this.squares = [];
-			this.bouldersToGo = level.boulders;
+			this.bouldersToGo = level.boulders; /* the property boulders holds the number of boulders in the level */
 
 			for (var y = 0; y < level.field.length; y++) {
 				var line = level.field[y];
@@ -68,16 +72,22 @@ var SokobanField = {
 				for (var x = 0; x < line.length; x++) {
 					var tableCell = dom("TD");
 					tableRow.appendChild(tableCell);
-					var square = Square.create(line.charAt(x), tableCell);
+					/**
+					 * The code in the book var square = Square.create(line.charAt(x), tableCell);
+					 * does not work. So I changed it using Object.create and then square.construct
+					 */
+					
+					var square = Object.create(Square);
+						square.construct(line.charAt(x), tableCell);
 					squareRow.push(square);
 					if (square.hasPlayer())
-						this.playerPos = new Point(x, y);
+						this.playerPos = new Point(x, y); /* Point is defined in terrarium.js */
 				}
-				tbody.appendChild(tableRow);
+				tbody.appendChild(tableRow); /* row added to table body */
 				this.squares.push(squareRow);
 			}
 
-			this.table = dom("TABLE", {"class": "sokoban"}, tbody);
+			this.table = dom("TABLE", {"class": "sokoban"}, tbody); /* A TABLE node is created with class sokoban and tbody is added as child */
 			this.score = dom("DIV", null, "...");
 			this.updateScore();
 		},
@@ -93,3 +103,167 @@ var SokobanField = {
 			return this.bouldersToGo <= 0;
 		}
 };
+
+SokobanField.place = function(where) {
+	where.appendChild(this.score);
+	where.appendChild(this.table);
+};
+SokobanField.remove = function() {
+	removeElement(this.score);
+	removeElement(this.table);
+	}; 
+
+SokobanField.move = function(direction){
+		goingto = this.playerPos.add(direction); /* point where I'm trying to go */
+		goingtosquare = this.getSquare(goingto);
+		playersquare = this.getSquare(this.playerPos);
+		if (goingtosquare.isEmpty()){
+				console.log("Moving player into empty square...");
+				playersquare.moveContent(goingtosquare);
+				playersquare.clearContent();
+				this.playerPos = goingto;
+		}
+		else {
+			if (goingtosquare.hasBoulder()){
+				console.log("There is a boulder ...");
+				beyondBoulder = goingto.add(direction);
+				beyondBouldersquare = this.getSquare(beyondBoulder);
+				if (beyondBouldersquare.isExit()){
+					console.log("There is an exit ...");
+					this.bouldersToGo--;
+					this.updateScore();
+					goingtosquare.clearContent();
+					playersquare.moveContent(goingtosquare);
+					playersquare.clearContent();
+					this.playerPos = goingto;
+				}
+				if (beyondBouldersquare.isEmpty()){
+					console.log("Just push the boulder ...");
+					goingtosquare.moveContent(beyondBouldersquare);
+					goingtosquare.clearContent();
+					playersquare.moveContent(goingtosquare);
+					playersquare.clearContent();
+					this.playerPos = goingto;
+				}
+			}
+			
+		}
+
+	};
+	
+	var SokobanGame = {
+			construct: function(place) {
+				this.level = null;
+				this.field = null;
+
+				var newGame = dom("BUTTON", null, "New game");
+				addHandler(newGame, "click", method(this, "newGame"));
+				var reset = dom("BUTTON", null, "Reset level");
+				addHandler(reset, "click", method(this, "reset"));
+				this.container = dom("DIV", null,
+						dom("H1", null, "Sokoban"),
+						dom("DIV", null, newGame, " ", reset));
+				place.appendChild(this.container);
+
+				addHandler(document, "keydown", method(this, "keyDown"));
+				this.newGame();
+			},
+
+			newGame: function() {
+				this.level = 0;
+				this.reset();
+			},
+			reset: function() {
+				if (this.field)
+					this.field.remove();
+				var sokobanField = Object.create(SokobanField);
+				sokobanField.construct(sokobanLevels[this.level]);
+				sokobanField.place(this.container);
+			},
+
+			keyDown: function(event) {
+				// To be filled in
+			}
+	};
+	
+/** sokobanLevels represents the levels of the game in text format
+ * for some reason when I copied it from the book most of the spaces are cut.
+ * So I amended just the first value adding the spaces. Not all the others.
+ * It means just the first level will render properly on the screen
+ */	
+	var sokobanLevels = [
+{field: ["###### ##### ",
+         "#   #   #  # ",
+         "# 0 #### 0 # ",
+         "# 0  @  0  # ",
+         "#  ######0 # ",
+         "####  ### ###",
+         "        #   #",
+         "       #0   #",
+         "       # 0  #",
+         "      ## 0  #",
+         "      #*0 0 #",
+         "     ########"],
+boulders: 10},
+{field: ["########### ",
+"# # # ",
+"# 00#00 @# ",
+"# 0 # ",
+"# # # ",
+"## ######### ",
+"# 0 # # ",
+"# 00 #0 0 0# ",
+"# 0 0 # ",
+"# 000#0 0 ###",
+"# # 0 0 *#",
+"##############"],
+boulders: 20},
+{field: ["########## ",
+"#@ *# ",
+"# ## ",
+"####### ######",
+" # #",
+" # 0 0 0 0 0 #",
+"######## #####",
+"# 0 0 0 0 #",
+"# 0 #",
+"##### ########",
+" # 0 0 0 # ",
+" # 0 # ",
+" # 0 0 0 ## ",
+"####### #### ",
+"# 0 # ",
+"# # ",
+"# ###### ",
+"##### "],
+boulders: 16},
+{field: [" #### ",
+"## @######## ",
+"# # ",
+"# 0#####0# # ",
+"# # # 0 # ",
+"# 0 0 0## ",
+"# 0 0 # # ",
+"# ####0 ## # ",
+"# 0 0 # ## ",
+"# ###0# 0 ##",
+"# # 0# 0 *#",
+"# 0 ####",
+"##### # # ",
+" ####### "],
+boulders: 12},
+{field: ["###### #####",
+"# #*## ## #",
+"# #### 0 #",
+"# 00 # # 0 #",
+"## 00# 00 ##",
+" #0 0 #0 # ",
+" # 00 # # 0# ",
+" # 0 0#### 0 # ",
+" # # ## ",
+" #### 0 # ## ",
+" ### ## # ",
+" # 0 # ",
+" #@ # # ",
+" ####### "],
+boulders: 18}]; 
