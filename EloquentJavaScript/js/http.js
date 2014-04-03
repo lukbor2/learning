3c301340e55c5ced4be6f6d877748022bb528a42
OpenShift guest <lukbor2@gmail.com>
2014-04-03 13:46:47 -0400

    other stuff in Chapter 14 from Cloud9

--- EloquentJavaScript/js/http.js
+++ EloquentJavaScript/js/http.js
@@ -14,3 +14,15 @@ function makeHttpObject() {
 
     throw new Error("Could not create HTTP request object.");
 }
+
+function simpleHttpRequest(url, success, failure) {
+    var request = makeHttpObject();
+    request.open("GET", url, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if (request.status == 200) success(request.responseText);
+            else if (failure) failure(request.status, request.statusText);
+        }
+    };
+}
