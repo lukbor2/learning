e9507dc8547c0d3aad8291a21b16696d93d70c2d
lukbor2 <lukbor2@gmail.com>
2015-11-21 15:19:47 -0800

    Saturday 11/21/2015

--- Python/Functions and Generators/arguments_intersect.py
+++ Python/Functions and Generators/arguments_intersect.py
@@ -15,3 +15,11 @@ s3="SLAM"
 
 print(intersect(s1,s2))
 print(intersect([1,2,3], (1,4)))
+
+def tester(func, items, trace=True):
+    for i in range(len(items)):
+        items = items[1:] + items[:1]
+        if trace: print(items)
+        print(sorted(func(*items)))
+        
+tester(intersect, ('a','abcdefg','abdst','albmcd'))
