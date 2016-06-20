5cedf37ca4017e6649e7a849e5a867db9550cafd
lukbor2 <lukbor2@gmail.com>
2016-06-19 19:40:33 -0700

    django tango end of setup chapter

--- Python/tango_with_django_project/rango/urls.py
+++ Python/tango_with_django_project/rango/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
-	url(r'^.*$', views.index, name = 'index')) # As reg-ex I could have used just ^$ too.
+	url(r'^$', views.index, name = 'index'),
+	url(r'^about/', views.about, name = 'about'),
+	) 
--- Python/tango_with_django_project/rango/views.py
+++ Python/tango_with_django_project/rango/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says Hi There World")
+	content_returned = 'Rango says, Hi There World. <br> <a href="/rango/about/">Click Here for about page</a>'
+	return HttpResponse(content_returned)
+
+def about(request):
+	content_returned = 'Rango says, This is the about page. <br> <a href="/rango">Back to Home Page</a>'
+	return HttpResponse(content_returned)
+
 
