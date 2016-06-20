d38f578e9bc1cc1f71536614d2a0851ea3c7ecab
lukbor2 <lukbor2@gmail.com>
2016-06-19 19:21:15 -0700

    added RegEx.txt file

--- Python/tango_with_django_project/rango/urls.py
+++ Python/tango_with_django_project/rango/urls.py
@@ -2,4 +2,4 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name = 'index'))
+	url(r'^.*$', views.index, name = 'index')) # As reg-ex I could have used just ^$ too.
