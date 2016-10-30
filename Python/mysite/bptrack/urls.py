6d129acb499e4235b54c58e3e37aa27733b418ab
lukbor2 <lukbor2@gmail.com>
2016-10-30 00:31:24 -0700

    bptrack, added some views.

--- Python/mysite/bptrack/urls.py
+++ Python/mysite/bptrack/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
 from django.conf import settings
 from bptrack import views
-# from books.views import PublisherList, PublisherDetail
+from bptrack.views import PatientList, PatientDetail, PatientBPMeasure
 
 app_name = 'bptrack'
 
 urlpatterns = [
                
     #url(r'^search-form/$', views.search_form),
-    url(r'^home/$', views.home),
+    url(r'^home/$', PatientList.as_view()),
+    url(r'^patient_detail/(?P<pk>[0-9]+)$', PatientDetail.as_view()),
+    url(r'^patient_bpmeasure/([0-9]+)$', PatientBPMeasure.as_view()),
 ]
 
 if settings.DEBUG:
--- Python/mysite/bptrack/views.py
+++ Python/mysite/bptrack/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponse, HttpResponseRedirect
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 from bptrack.models import Patient, BP_Measure
 
+class PatientList(ListView):
+    model = Patient
+    template_name = 'bptrack_home.html'
 
-def home(request):
-    return render(request, 'bptrack_home.html')
+    # Instead of using the standard context name object_list, I am setting my own name for clarity
+    context_object_name = 'all_patients'
 
+class PatientDetail(DetailView):
+    # General Note: the detail view seems to automatically expect the primary key of the model passed via the url.
+    # I don't have to do anything special to filter the object corresponding to the primary key.
+    
+    model = Patient
+    template_name = 'bptrack_patient_detail.html'
+    
+    context_object_name = 'patient_detail'
+    
+class PatientBPMeasure(ListView):
+    """
+    In this view I show the list of the measures filtered on a patient whose id is passed via the URL.
+    Furthermore I make the patient data available in the context, so I can show these data in the template.
+    """
+    
+    
+    template_name = 'bptrack_patient_measures.html'
+    context_object_name = 'patient_measure'
+    
+    def get_queryset(self):
+        self.patient = get_object_or_404(Patient, id=self.args[0]) 
+        return BP_Measure.objects.filter(patient = self.patient)
+    
+    def get_context_data(self, **kwargs):
+        context = super(PatientBPMeasure, self).get_context_data(**kwargs)
+        context['patient'] = self.patient
+        return context
+    
 
 def debug(request):
     pass
