cf81a630498fbd8505692be78491c746d67bddf8
lukbor2 <lukbor2@gmail.com>
2017-04-23 22:40:31 -0700

    Created a version 2

--- Python/TestWithSelenium/check_links_v01.py
+++ Python/TestWithSelenium/check_links_v01.py
@@ -1,8 +1,6 @@
 import requests
+import time
 from bs4 import BeautifulSoup
-
-file_name = 'links_check.txt' #name of the text file where results are written.
-site = 'https://www.ghirardelli.com'
 
 def get_urls(page):
     #returns a list of non-duplicated urls from the page.
@@ -23,24 +21,50 @@ def process_list(urls):
     return urls_to_check
 
 def check_urls(urls):
-    #TODO
-    pass
+    results = []
+    for url in urls:
+        print("Now Processing: " + str(url) + "\n")
+        try:
+            r = requests.get(url)
+            if r.history:
+                history_status_codes = [str(h.status_code) for h in r.history]
+                result = (r.status_code, r.history, url, 'Success. Redirect to ' + r.url)
+            elif r.status_code == 200:
+                result = (r.status_code, r.history, url, 'Success. No Redirect')
+            else:
+                result = (r.status_code, r.history, url, 'ERROR')
+        except Exception as e:
+            result = (0, [], url, e)
+        results.append(result)
+    return results
 
-urls_list = []
-urls_list.append(site) #starting point is just the url of the site.
-urls_to_check = [] #this is where I store all the urls which will have to be checked.
-depth = 2 #I use this variable to control how many iterations I will do. i.e. how deep in the tree I will go to check the links.
-i = 0
-while i < depth:
-    urls = process_list(urls_list)
-    urls_to_check += urls
-    urls_list = urls
-    i += 1
+if __name__ == "__main__":
+    file_name = 'links_check.txt' #name of the text file where results are written.
+    site = 'https://www.ghirardelli.com'
+    urls_list = []
+    urls_list.append(site) #starting point is just the url of the site.
+    urls_to_check = [] #this is where I store all the urls which will have to be checked.
+    depth = 1 #I use this variable to control how many iterations I will do. i.e. how deep in the tree I will go to check the links.
+    i = 0
+    while i < depth:
+        urls = process_list(urls_list)
+        urls_to_check += urls
+        urls_list = urls
+        i += 1
+    urls_to_check = list(set(urls_to_check)) #removing duplicates
 
-try:
-    f = open(file_name, 'w')
-except OSError:
-    print('ERROR opening the file where to write results.')
+    try:
+        f = open(file_name, 'w')
+    except OSError:
+        print('ERROR opening the file where to write results.')
 
-for result in urls_to_check:
-    f.write(str(result) + '\n')
+    for result in urls_to_check:
+        f.write(str(result) + '\n')
+
+    time_start = time.time()
+    for result in check_urls(urls_to_check):
+        f.write(str(result) + '\n')
+    time_end = time.time()
+    print("Execution time - Version 1: " + time_start - time_end)
+
+    f.close()
