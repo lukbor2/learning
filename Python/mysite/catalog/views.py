6c70d2903ea89710b7d372b2efd36f820b0db53b
lukbor2 <lukbor2@gmail.com>
2018-01-12 14:09:15 -0600

    Catalog - Started the chapter on Views.

--- Python/mysite/catalog/views.py
+++ Python/mysite/catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from models import Author, Book, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+	"""
+		View function for the home page of the catalog website.
+	"""
+	#Generate counts of some of the main objects.
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+	
+	num_authors=Author.objects.count()  # The 'all()' is implied by default.
+
+	#Available books
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+	
+	# Render the HTML template index.html with the data in the context variable
+	return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+    )
