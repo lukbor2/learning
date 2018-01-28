3a77d8511c2a0e195d9c12dc94482533d13fc255
lukbor2 <lukbor2@gmail.com>
2018-01-28 16:52:23 -0600

    changed models in Books

--- Python/mysite/books/models.py
+++ Python/mysite/books/models.py
@@ -10,13 +10,13 @@ class Publisher (models.Model):
     state_province = models.CharField(max_length=30)
     country = models.CharField(max_length=50)
     website = models.URLField()
-    
+
     class Meta:
         ordering = ["-name"]
-    
+
     def __str__(self):
         return self.name
-    
+
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
@@ -24,22 +24,21 @@ class Author(models.Model):
     salutation = models.CharField(max_length=10)
     name = models.CharField(max_length=200)
     # headshot = models.ImageField(upload_to='author_headshots')
-    
+
     def __str__(self):
         return u'%s %s' % (self.first_name, self.last_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
-    publisher = models.ForeignKey(Publisher)
+    publisher = models.ForeignKey(Publisher,on_delete=models.SET_NULL,))
     publication_date = models.DateField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.title
-    
+
     def _get_book_info(self):
         """Example of a model method."""
         return '%s %s %s' % (self.title, self.publisher, self.publication_date.strftime('%m%d%Y'))
-    
+
     book_info = property(_get_book_info)
-       
