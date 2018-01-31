77084453756d611cedeb0cfadc2be5f067fc7dcc
lukbor2 <lukbor2@gmail.com>
2018-01-31 12:58:03 -0600

    Changed forms in books

--- Python/mysite/books/forms.py
+++ Python/mysite/books/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     email = forms.EmailField(required=False, label = 'Your e-mail address')
-    message = forms.CharField(widget=forms.Textarea)
+    message = forms.CharField(max_length=500, widget=forms.Textarea)
     
     def clean_message(self):
         message = self.cleaned_data['message']
