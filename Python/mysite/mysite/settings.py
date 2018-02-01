524f813084d6f844334d1239993d0eea373309f8
lukbor2 <lukbor2@gmail.com>
2018-01-31 22:02:37 -0600

    working on login

--- Python/mysite/mysite/settings.py
+++ Python/mysite/mysite/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates',],
+        'DIRS': ['./templates', './templates/registration',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
