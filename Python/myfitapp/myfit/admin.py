7f144ba17f4f775f8fc427694016129710ca8ceb
lukbor2 <lukbor2@gmail.com>
2018-02-25 22:22:36 -0600

    working on admin site for fitapp

--- Python/myfitapp/myfit/admin.py
+++ Python/myfitapp/myfit/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 
 from .models import Device, Device_Owner
 
-admin.site.register(Device)
-
 class Device_OwnerAdmin(admin.ModelAdmin):
     fields = ('name', 'surname', 'date_of_birth', 'age', 'sex','heart_rate_rest')
-    list_display = ('name', 'surname', 'date_of_birth', 'age', 'sex', 'heart_rate_rest')
+    list_display = ('name', 'surname', 'date_of_birth', 'age', 'sex', 'height', 'weight', 'heart_rate_max', 'heart_rate_rest')
 
 admin.site.register(Device_Owner, Device_OwnerAdmin)
+
+class DeviceAdmin(admin.ModelAdmin):
+    fields = ('device_serial_no', 'device_name', 'device_model', 'device_owner')
+    list_display = ('device_serial_no', 'device_name', 'device_model')
+
+admin.site.register(Device, DeviceAdmin)
